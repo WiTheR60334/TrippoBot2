32b3ed22559643d3be2fade161c3c1af726cbbe1
WiTheR <115364885+WiTheR60334@users.noreply.github.com>
2024-12-26 19:09:30 +0530

    exp

--- app/chat-diamond/page.jsx
+++ app/chat-diamond/page.jsx
@@ -1,3 +1,6 @@
+export const maxDuration = 60;
+export const dynamic = 'force-dynamic';
+
 import ChatBotGold from "@/components/ChatBotGold/ChatBotGold"; // Adjust the path based on your structure
 import ChatBotDiamond from "../../components/ChatBotDiamond/ChatBotDiamond";
 
