7637745ea46c2ee98a6da310bb9cbe4dd8fb341e
WiTheR <115364885+WiTheR60334@users.noreply.github.com>
2024-12-26 20:02:26 +0530

    exp4

--- app/chat-diamond/page.jsx
+++ app/chat-diamond/page.jsx
@@ -1,5 +1,7 @@
+"use client";
 import { unstable_noStore as noStore } from 'next/cache';
 export const dynamic = 'force-dynamic';
+export const maxDuration = 60;
 
 import ChatBotGold from "@/components/ChatBotGold/ChatBotGold"; // Adjust the path based on your structure
 import ChatBotDiamond from "../../components/ChatBotDiamond/ChatBotDiamond";
