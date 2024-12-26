3bd2b621132f64f76e0ef9d2df4c76ddc15740ab
WiTheR <115364885+WiTheR60334@users.noreply.github.com>
2024-12-26 19:21:00 +0530

    exp2

--- app/chat-diamond/page.jsx
+++ app/chat-diamond/page.jsx
@@ -1,10 +1,10 @@
-export const maxDuration = 60;
-export const dynamic = 'force-dynamic';
+import { unstable_noStore as noStore } from 'next/cache';
 
 import ChatBotGold from "@/components/ChatBotGold/ChatBotGold"; // Adjust the path based on your structure
 import ChatBotDiamond from "../../components/ChatBotDiamond/ChatBotDiamond";
 
 export default function ChatPage() {
+  noStore();
   return (
     <div className="min-h-screen bg-gray-100">
       <ChatBotDiamond />
